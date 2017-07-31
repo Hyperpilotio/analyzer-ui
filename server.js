253558a0bc478fb07bc6dc01e35bd37308521b95
adrianliaw <adrianliaw2000@gmail.com>
2017-07-31 12:00:13 -0700

    Create simple express app that serves the webpack builds and proxies api

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const proxy = require("express-http-proxy");
+const path = require("path");
+const config = require("./config.json");
+
+
+const ANALYSIS_APP = process.env.ANALYSIS_APP || "sizing-analysis";
+
+const server = express();
+
+server.use("/static", express.static(path.join(__dirname, "dist/static")));
+
+server.use("/api", proxy(config.analyzer.url, {
+  proxyReqPathResolver: req => req.originalUrl
+}));
+
+server.get("/*", (req, res) => {
+  res.sendFile(__dirname + `/dist/${ANALYSIS_APP}.html`);
+});
+
+server.listen(3000, () => {
+  console.log(`${ANALYSIS_APP} UI app listening on port 3000!`);
+});
