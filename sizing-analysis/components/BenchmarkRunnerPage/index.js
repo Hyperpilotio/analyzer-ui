89796a30da9a8cf1137bb4d259f7c8527a6efa70
adrianliaw <adrianliaw2000@gmail.com>
2017-08-01 15:00:43 -0700

    Add introduction in jumbotron

--- sizing-analysis/components/BenchmarkRunnerPage/index.js
+++ sizing-analysis/components/BenchmarkRunnerPage/index.js
@@ -3,9 +3,7 @@ import TestStagesJumbotron from "../TestStagesJumbotron";
 
 const BenchmarkRunnerPage = () => (
   <div>
-    <TestStagesJumbotron>
-      <h3>Initial Benchmark Test</h3>
-    </TestStagesJumbotron>
+    <TestStagesJumbotron />
   </div>
 );
 
--- sizing-analysis/components/TestStagesJumbotron/index.js
+++ sizing-analysis/components/TestStagesJumbotron/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import Container from "../../../commons/components/Container";
 import styles from "./index.scss";
 
-const TestStagesJumbotron = ({ children }) => (
+const TestStagesJumbotron = () => (
   <div className={styles.TestStagesJumbotron}>
-    <Container>
-      { children }
+    <Container className={styles.Container}>
+      <div className={styles.Intro}>
+        <h3>Initial Benchmark Test</h3>
+        <p>Connect existing apps from cluster managers to run analyzer</p>
+      </div>
     </Container>
   </div>
 );
