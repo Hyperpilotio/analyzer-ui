14eda02699a8240e45c9556bb0fedba504c0dfaf
adrianliaw <adrianliaw2000@gmail.com>
2017-08-01 16:48:52 -0700

    Add test stage indicators to the jumbotron

--- sizing-analysis/components/TestStagesJumbotron/index.js
+++ sizing-analysis/components/TestStagesJumbotron/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Container from "../../../commons/components/Container";
+import BenchmarkStageIndicator from "../BenchmarkStageIndicator";
 import styles from "./index.scss";
+import hyperpilotNavLogo from "../../../assets/images/asset_hyperpilot_nav_logo.svg";
 
 const TestStagesJumbotron = () => (
   <div className={styles.TestStagesJumbotron}>
     <Container className={styles.Container}>
-      <div className={styles.Intro}>
+      <div className={styles.intro}>
         <h3>Initial Benchmark Test</h3>
         <p>Connect existing apps from cluster managers to run analyzer</p>
+      </div>
+      <div className={styles.stages}>
+        <BenchmarkStageIndicator
+          title="Benchmark Test"
+          description="all your instances"
+          icon={hyperpilotNavLogo} />
+        <span className={styles["arrow-icon"]} />
+        <BenchmarkStageIndicator
+          title="Recommend"
+          description="instances for apps"
+          icon={hyperpilotNavLogo} />
       </div>
     </Container>
   </div>
