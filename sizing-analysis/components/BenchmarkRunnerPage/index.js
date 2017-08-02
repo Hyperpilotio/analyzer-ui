13e515e155420997907cef2a5d306a42580f1063
adrianliaw <adrianliaw2000@gmail.com>
2017-08-02 08:03:35 -0700

    Add cluster infos

--- sizing-analysis/components/BenchmarkRunnerPage/index.js
+++ sizing-analysis/components/BenchmarkRunnerPage/index.js
@@ -1,10 +1,32 @@
 import React from "react";
 import TestStagesJumbotron from "../TestStagesJumbotron";
+import Container from "commons/components/Container";
 import { STAGE_CONFIG, STAGE_TEST, STAGE_RESULT } from "../../constants";
+import styles from "./index.scss";
 
 const BenchmarkRunnerPage = () => (
-  <div>
+  <div className={styles.BenchmarkRunnerPage}>
     <TestStagesJumbotron stage={STAGE_CONFIG} />
+    <main>
+      <Container>
+        <div>
+          <h3>Existing Apps</h3>
+          <div className={styles["cluster-info"]}>
+            <p>Cluster manager</p>
+            <h3>AWS EC2</h3>
+          </div>
+          <div className={styles["cluster-info"]}>
+            <p>Service ID</p>
+            <h3>3902b203af</h3>
+          </div>
+          <div className={styles["cluster-info"]}>
+            <p>Total apps running</p>
+            <h3>6</h3>
+          </div>
+        </div>
+        <div></div>
+      </Container>
+    </main>
   </div>
 );
 
