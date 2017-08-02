a6ca40355e64353ed29f0926ee5558e2ea09c81e
adrianliaw <adrianliaw2000@gmail.com>
2017-08-02 16:47:33 -0700

    Fix test stages jumbotron not responding to stage change

--- sizing-analysis/components/TestStageIndicator/index.js
+++ sizing-analysis/components/TestStageIndicator/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from "./index.scss";
 
-const TestStageIndicator = ({ title, description, icon, active = false }) => {
+const TestStageIndicator = ({
+  title,
+  description,
+  icon,
+  activeIcon,
+  active = false
+}) => {
   let activeClass = active ? styles.active : "";
+  let iconUrl = active ? activeIcon : icon;
   return <div className={styles.TestStageIndicator}>
-    <img className={`${styles.element} ${styles.icon} ${activeClass}`} src={icon} />
+    <img
+      className={`${styles.element} ${styles.icon} ${activeClass}`}
+      src={iconUrl} />
     <div className={`${styles.element} ${styles.content} ${activeClass}`}>
       <h4> { title } </h4>
       <p> { description } </p>
