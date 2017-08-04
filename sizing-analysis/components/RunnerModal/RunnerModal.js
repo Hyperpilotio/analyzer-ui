dec37d81168e83fd7cb348bf08787f6eef19bc7e
adrianliaw <adrianliaw2000@gmail.com>
2017-08-04 08:41:31 -0700

    Add see results button

--- sizing-analysis/components/RunnerModal/RunnerModal.js
+++ sizing-analysis/components/RunnerModal/RunnerModal.js
@@ -1,31 +1,37 @@
 import React from "react";
 import FaCheck from "react-icons/lib/fa/check";
 import FaRefresh from "react-icons/lib/fa/refresh";
+import { Link } from "react-router-dom";
 import ProgressBar from "commons/components/ProgressBar";
 import ProgressItem from "../ProgressItem";
+import Button from "commons/components/Button";
 import styles from "./index.scss";
 
-const sampleProgress = [
-  { status: "completed", instance: "C3.large", time: 25 },
-  { status: "completed", instance: "C3.xlarge", time: 21 },
-  { status: "completed", instance: "M2.xlarge", time: 10 },
-  { status: "running", instance: "M3.large", time: 15 },
-  { status: "running", instance: "G4.large", time: 9 },
-  { status: "running", instance: "G3.xlarge", time: 20 }
-];
-
-const RunnerModal = ({ className = "" }) => (
+const RunnerModal = ({
+  tasksProgress = [],
+  progress = 100,
+  remainingTime = 0,
+  finished = false,
+  className = ""
+}) => (
   <div className={`${styles.RunnerModal} ${className}`}>
     <h3>Running sizing analysis...</h3>
     <div className={styles.progressbar}>
-      <ProgressBar percent={50} />
-      <p>20mins left</p>
+      <ProgressBar percent={progress} />
+      <p>{ finished ? "Done!" : `${remainingTime}mins left` }</p>
     </div>
     <ul className={styles["completion-status-group"]}>
-      { sampleProgress.map( ({ status, instance, time }) => (
+      { tasksProgress.map( ({ status, instance, time }) => (
         <ProgressItem status={status} instance={instance} runningTime={time} />
       ) ) }
     </ul>
+    { finished ? (
+      <div className={styles["button-to-result"]}>
+        <Link to="/sizing-test/result" className={styles.Button}>
+          See Results
+        </Link>
+      </div>
+    ) : null }
   </div>
 );
 
--- sizing-analysis/components/SizingRunnerPage/index.js
+++ sizing-analysis/components/SizingRunnerPage/index.js
@@ -6,10 +6,23 @@ import Container from "commons/components/Container";
 import { STAGE_CONFIG, STAGE_TEST, STAGE_RESULT } from "../../constants";
 import styles from "./index.scss";
 
+const sampleProgress = [
+  { status: "completed", instance: "C3.large", time: 25 },
+  { status: "completed", instance: "C3.xlarge", time: 21 },
+  { status: "completed", instance: "M2.xlarge", time: 10 },
+  { status: "running", instance: "M3.large", time: 15 },
+  { status: "running", instance: "G4.large", time: 9 },
+  { status: "running", instance: "G3.xlarge", time: 20 }
+];
+
 const SizingRunnerPage = ({ stage = STAGE_CONFIG }) => {
   let modalElement = stage !== STAGE_TEST ? "" : (
     <div className={styles["modal-container"]}>
-      <RunnerModal />
+      <RunnerModal
+        tasksProgress={sampleProgress}
+        progress={60}
+        remainingTime={20}
+        finished />
     </div>
   );
 
