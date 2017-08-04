e55f37d4ac4d9fe6d037f9e7abd62eb8f61eb9dd
adrianliaw <adrianliaw2000@gmail.com>
2017-08-03 17:59:00 -0700

    Use better file naming for RunnerModal for styleguidist

---
+++ sizing-analysis/components/RunnerModal/RunnerModal.js
@@ -0,0 +1,32 @@
+import React from "react";
+import FaCheck from "react-icons/lib/fa/check";
+import FaRefresh from "react-icons/lib/fa/refresh";
+import ProgressBar from "commons/components/ProgressBar";
+import ProgressItem from "../ProgressItem";
+import styles from "./index.scss";
+
+const sampleProgress = [
+  { status: "completed", instance: "C3.large", time: 25 },
+  { status: "completed", instance: "C3.xlarge", time: 21 },
+  { status: "completed", instance: "M2.xlarge", time: 10 },
+  { status: "running", instance: "M3.large", time: 15 },
+  { status: "running", instance: "G4.large", time: 9 },
+  { status: "running", instance: "G3.xlarge", time: 20 }
+];
+
+const RunnerModal = ({ className = "" }) => (
+  <div className={`${styles.RunnerModal} ${className}`}>
+    <h3>Running sizing analysis...</h3>
+    <div className={styles.progressbar}>
+      <ProgressBar percent={50} />
+      <p>20mins left</p>
+    </div>
+    <ul className={styles["completion-status-group"]}>
+      { sampleProgress.map( ({ status, instance, time }) => (
+        <ProgressItem status={status} instance={instance} runningTime={time} />
+      ) ) }
+    </ul>
+  </div>
+);
+
+export default RunnerModal;
