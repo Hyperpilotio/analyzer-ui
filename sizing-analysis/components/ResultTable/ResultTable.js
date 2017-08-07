f6d591e19f66e6627794b584910588226df0ca82
adrianliaw <adrianliaw2000@gmail.com>
2017-08-07 15:00:54 -0700

    Move results table content into ResultTable component

--- sizing-analysis/components/ResultTable/ResultTable.js
+++ sizing-analysis/components/ResultTable/ResultTable.js
@@ -1,12 +1,42 @@
 import React from "react";
-import Container from "commons/components/Container";
 import styles from "./ResultTable.scss";
 
-const ResultTable = ({ className = "", containerClassName = "", children }) => (
+const ResultTable = ({ className = "" }) => (
   <div className={`${styles.ResultTable} ${className}`}>
-    <Container className={`${styles.Container} ${containerClassName}`}>
-      { children }
-    </Container>
+    <div className={styles["bench-grid"]}>
+      <table>
+        <tr className={styles["grid-tr"]}>
+          <th className={styles["grid-th"]}></th>
+          <th className={styles["grid-th"]}>Instance Type</th>
+          <th className={styles["grid-th"]}>Perf</th>
+          <th className={styles["grid-th"]}>Cost</th>
+        </tr>
+        <tr className={styles["grid-tr"]}>
+          <td className={styles["grid-td"]}><div className={styles["oval-2"]}></div>Optimal Perf/Cost</td>
+          <td className={styles["grid-td"] + " " + styles["center"]}>c4.large</td>
+          <td className={styles["grid-td"] + " " + styles["center"]}>600</td>
+          <td className={styles["grid-td"] + " " + styles["center"]}>$230.78</td>
+        </tr>
+        <tr className={styles["grid-tr"]}>
+          <td className={styles["grid-td"] }><div className={styles["oval-3"]}></div>High performance</td>
+          <td className={styles["grid-td"] + " " + styles["center"]}>c3.X-large</td>
+          <td className={styles["grid-td"] + " " + styles["center"]}>680</td>
+          <td className={styles["grid-td"] + " " + styles["center"]}>$250.43</td>
+        </tr>
+        <tr className={styles["grid-tr"]}>
+          <td className={styles["grid-td"] + " " + styles["left"]}><div className={styles["oval-4"]}></div>Low cost</td>
+          <td className={styles["grid-td"] + " " + styles["center"]}>m2.medium</td>
+          <td className={styles["grid-td"] + " " + styles["center"]}>270</td>
+          <td className={styles["grid-td"] + " " + styles["center"]}>$60.25</td>
+        </tr>
+
+      </table>
+    </div>
+    <div >
+      <div>
+        <h3></h3>
+      </div>
+    </div>
   </div>
 );
 
--- sizing-analysis/components/SizingResultsPage/SizingResultsPage.js
+++ sizing-analysis/components/SizingResultsPage/SizingResultsPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Jumbotron from "commons/components/Jumbotron";
+import Container from "commons/components/Container";
 import Navbar from "commons/components/Navbar";
 import NavItemLink from "commons/components/NavItemLink";
 import ResultTable from "../ResultTable";
@@ -51,42 +52,9 @@ const SizingResultsPage = () => (
         <img src={kafkaLogo} /> Kafka
       </NavItemLink>
     </Navbar>
-    <ResultTable>
-      <div className={styles["bench-grid"]}>
-        <table>
-          <tr className={styles["grid-tr"]}>
-            <th className={styles["grid-th"]}></th>
-            <th className={styles["grid-th"]}>Instance Type</th>
-            <th className={styles["grid-th"]}>Perf</th>
-            <th className={styles["grid-th"]}>Cost</th>
-          </tr>
-          <tr className={styles["grid-tr"]}>
-            <td className={styles["grid-td"]}><div className={styles["oval-2"]}></div>Optimal Perf/Cost</td>
-            <td className={styles["grid-td"] + " " + styles["center"]}>c4.large</td>
-            <td className={styles["grid-td"] + " " + styles["center"]}>600</td>
-            <td className={styles["grid-td"] + " " + styles["center"]}>$230.78</td>
-          </tr>        
-          <tr className={styles["grid-tr"]}>
-            <td className={styles["grid-td"] }><div className={styles["oval-3"]}></div>High performance</td>
-            <td className={styles["grid-td"] + " " + styles["center"]}>c3.X-large</td>
-            <td className={styles["grid-td"] + " " + styles["center"]}>680</td>
-            <td className={styles["grid-td"] + " " + styles["center"]}>$250.43</td>
-          </tr>        
-          <tr className={styles["grid-tr"]}>
-            <td className={styles["grid-td"] + " " + styles["left"]}><div className={styles["oval-4"]}></div>Low cost</td>
-            <td className={styles["grid-td"] + " " + styles["center"]}>m2.medium</td>
-            <td className={styles["grid-td"] + " " + styles["center"]}>270</td>
-            <td className={styles["grid-td"] + " " + styles["center"]}>$60.25</td>
-          </tr>                
-
-        </table>
-      </div>
-      <div >
-        <div>
-          <h3></h3>
-        </div>
-      </div>    
-    </ResultTable>
+    <Container className={styles["results-content"]}>
+      <ResultTable />
+    </Container>
   </div>
 );
 
