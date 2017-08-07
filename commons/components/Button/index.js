4570fcf162a9629ea354a3ac7f3ce5400f4b73a7
adrianliaw <adrianliaw2000@gmail.com>
2017-08-07 11:47:42 -0700

    Rename component filenames to component names

--- commons/components/Button/index.js
+++ commons/components/Button/index.js
@@ -1,13 +1,1 @@
-import React from "react";
-import styles from "./index.scss";
-
-const Button = ({ invert = false, className = "", onClick = () => {}, children }) => {
-  let invertClass = invert ? styles.invert : "";
-  return (
-    <a onClick={onClick} className={`${styles.Button} ${invertClass} ${className}`}>
-      { children }
-    </a>
-  );
-};
-
-export default Button;
+export { default } from "./Button";
--- commons/components/NavItemLink/index.js
+++ commons/components/NavItemLink/index.js
@@ -1,13 +1,1 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import _ from "lodash";
-import styles from "./index.scss";
-
-
-export default ({ className = "", activeClassName = "", to, text, children }) => (
-  <NavLink to={to}
-           className={`${styles.NavItemLink} ${className}`}
-           activeClassName={`${styles.selected} ${activeClassName}`}>
-    { _.isUndefined(children) ? text : children }
-  </NavLink>
-)
+export { default } from "./NavItemLink";
