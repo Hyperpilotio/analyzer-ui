bde4add0715ae3bd8575913932a8e904963b06bb
adrianliaw <adrianliaw2000@gmail.com>
2017-08-06 19:30:37 -0700

    Add navbar for sizing analysis results

--- sizing-analysis/components/SizingResultsPage/SizingResultsPage.js
+++ sizing-analysis/components/SizingResultsPage/SizingResultsPage.js
@@ -1,29 +1,56 @@
 import React from "react";
 import Jumbotron from "commons/components/Jumbotron";
+import Navbar from "commons/components/Navbar";
+import NavItemLink from "commons/components/NavItemLink";
 import KeySummary from "../KeySummary";
 import styles from "./index.scss";
+import mongoLogo from "assets/images/asset_mongoDB_logo.svg";
+import kafkaLogo from "assets/images/asset_kafka_logo.svg";
+import redisLogo from "assets/images/asset_redis_logo.svg";
 
 const SizingResultsPage = () => (
-  <Jumbotron>
-    <div className={styles["cluster-infos"]}>
-      <div>
-        <p>Cluster manager</p>
-        <h3>Kubernetes</h3>
+  <div>
+    <Jumbotron>
+      <div className={styles["cluster-infos"]}>
+        <div>
+          <p>Cluster manager</p>
+          <h3>Kubernetes</h3>
+        </div>
+        <div>
+          <p>Last update</p>
+          <h3>25/07/14</h3>
+        </div>
       </div>
-      <div>
-        <p>Last update</p>
-        <h3>25/07/14</h3>
+      <div className={styles["testing-summary"]}>
+        <p>Testing summary</p>
+        <main>
+          <KeySummary title="App tested" value="2" />
+          <KeySummary title="Running time" value="2:15" />
+          <KeySummary title="Instances ran" value="36" />
+        </main>
       </div>
-    </div>
-    <div className={styles["testing-summary"]}>
-      <p>Testing summary</p>
-      <main>
-        <KeySummary title="App tested" value="2" />
-        <KeySummary title="Running time" value="2:15" />
-        <KeySummary title="Instances ran" value="36" />
-      </main>
-    </div>
-  </Jumbotron>
+    </Jumbotron>
+    <Navbar>
+      <NavItemLink
+        to="/sizing-test/result/mongo"
+        className={styles.NavItemLink}
+        activeClassName={styles.selected}>
+        <img src={mongoLogo} /> mongoDB
+      </NavItemLink>
+      <NavItemLink
+        to="/sizing-test/result/redis"
+        className={styles.NavItemLink}
+        activeClassName={styles.selected}>
+        <img src={redisLogo} /> Redis
+      </NavItemLink>
+      <NavItemLink
+        to="/sizing-test/result/kafka"
+        className={styles.NavItemLink}
+        activeClassName={styles.selected}>
+        <img src={kafkaLogo} /> Kafka
+      </NavItemLink>
+    </Navbar>
+  </div>
 );
 
 export default SizingResultsPage;
