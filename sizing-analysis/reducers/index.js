092b280c483901cc4730604435b5f24bf7f4a036
thorhsu <thorshu@gmail.com>
2017-08-10 14:21:02 +0800

    restore persist again

--- sizing-analysis/reducers/index.js
+++ sizing-analysis/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import reducer from './sizingReducer';
 
 const rootReducer = combineReducers({
-  reducer
+  reducer: reducer
 })
 
 export default rootReducer
