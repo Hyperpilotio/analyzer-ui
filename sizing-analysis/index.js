e1e8540f7884772098b0ad6a228166f061a14758
adrianliaw <adrianliaw2000@gmail.com>
2017-08-11 16:31:59 -0700

    Use redux chrome devtools

--- sizing-analysis/index.js
+++ sizing-analysis/index.js
@@ -8,7 +8,13 @@ import reducers from './reducers';
 import "./index.scss";
 import { autoRehydrate, persistStore } from 'redux-persist';
 
-let store = createStore(reducers);
+let store = createStore(
+  reducers,
+  // Activate redux devtools in development mode
+  process.env.NODE_ENV === "production"
+    ? undefined
+    : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 persistStore(store);
 const rootEl = document.getElementById("react-root");
 const render = Component =>
