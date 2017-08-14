f92c6d11379865bffc647322c30e0429f8265b72
adrianliaw <adrianliaw2000@gmail.com>
2017-08-14 14:40:14 -0700

    Add simple tooltip

--- sizing-analysis/components/ResultFigure/ResultFigure.js
+++ sizing-analysis/components/ResultFigure/ResultFigure.js
@@ -4,127 +4,99 @@ import {
   VictoryScatter,
   VictoryAxis,
   VictoryLine,
-  VictoryZoomContainer
+  VictoryTooltip,
+  createContainer
 } from "victory";
 import styles from "./ResultFigure.scss";
-import { connect } from 'react-redux';
 import { mapStateToProps } from "../../actions";
 
 
 const WIDTH = 540;
 const HEIGHT = 400;
 
-const ResultFigure = ({ selectedApps, className, id }) => {
-  let selectedApp;
+const VictoryZoomVoronoiContainer = createContainer("zoom", "voronoi");
 
-  for (let app of selectedApps) {
-    if (id === app.appId) {
-      selectedApp = app;
-      break;
-    }
-  }
-  let optimal;
-  let highPerf;
-  let lowCost;
-  let budget;
-  let otherTests = [];
-
-  if (!!selectedApp) {
-    budget = selectedApp.budget;
-    if (!!selectedApp.sizingRuns) {
-      for (let sizingRun of selectedApp.sizingRuns) {
-        for (let test of sizingRun.results) {
-          otherTests.push(test);
-        }
-      }
-    }
-    if (!!selectedApp.recommendations) {
-      for (let result of selectedApp.recommendations) {
-        switch (result.objective) {
-          case "MaxPerfOverCost":
-            highPerf = result;
-            break;
-          case "MinCostWithPerfLimit":
-            lowCost = result;
-            break;
-          case "MaxPerfWithCostLimit":
-            optimal = result;
-            break;
-        }
-      }
-    }
-  }
-  return (
-    <div className={`${styles.ResultFigure} ${className}`}>
-      <VictoryChart
-        containerComponent={ <VictoryZoomContainer /> }
-        padding={0}
-        width={WIDTH}
-        height={HEIGHT}
-        domainPadding={{
-          x: [50, 50],
-          y: [50, 50]
-        }}
-      >
-        <VictoryLine
-          y={() => 50000}
-          style={{
-            data: {
-              strokeWidth: 1,
-              stroke: "#8cb1fa",
-              strokeDasharray: "5, 5"
-            }
-          }}
-        />
-        <VictoryLine
-          x={() => 300}
-          style={{
-            data: {
-              strokeWidth: 1,
-              stroke: "#ff8686",
-              strokeDasharray: "5, 5"
-            }
-          }}
-        />
-        <VictoryAxis dependentAxis style={{
-          axis: {
-            strokeWidth: 5,
-            stroke: "#5677fa"
-          },
-          grid: {
-            stroke: "#eef0fa"
-          }
-        }} />
-        <VictoryAxis style={{
-          axis: {
-            strokeWidth: 5,
-            stroke: "#5677fa"
-          }
-        }} />
-        <VictoryScatter
-          data={[
-            { x: 2442.24, y: 1920000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 3538.08, y: 1920000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 1234.08, y: 1920000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 2315.52, y: 2160000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 967.68, y: 1920000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 356.832, y: 960000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 1216.08, y: 1920000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 1120.392, y: 1920000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 362.88, y: 960000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 892.08, y: 2400000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 2959.2, y: 3840000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 289.44, y: 240000, fill: "#b9bacb", stroke: "#ffffff" },
-            { x: 273.6, y: 480000, fill: "#eef0fa", stroke: "#5677fa" }, //high performance
-            { x: 241.92, y: 480000, fill: "#daf9b8", stroke: "#b8e986" }, //low cost
-            { x: 725.76, y: 2160000, fill: "#5677fa", stroke: "#ffffff" }, //optimal
-          ]}
-          size={10}
-          style={{ data: { strokeWidth: 2 } }}
-        />
-      </VictoryChart>
-    </div>
-  );
+const pointStyles = {
+  maxPerfOverCost: { fill: "#5677fa", stroke: "#ffffff" },
+  maxPerfWithCostLimit: { fill: "#eef0fa", stroke: "#5677fa" },
+  minCostWithPerfLimit: { fill: "#daf9b8", stroke: "#b8e986" },
+  notSelected: { fill: "#b9bacb", stroke: "#ffffff" }
 };
 
-export default connect(mapStateToProps)(ResultFigure);
+class VictoryLineWithoutPoints extends VictoryLine {
+  static getData = () => null;
+}
+
+const ResultFigure = ({ className }) => (
+  <div className={`${styles.ResultFigure} ${className}`}>
+    <VictoryChart
+      containerComponent={ <VictoryZoomVoronoiContainer /> }
+      padding={0}
+      width={WIDTH}
+      height={HEIGHT}
+      domainPadding={{
+        x: [50, 50],
+        y: [50, 50]
+      }}
+    >
+      <VictoryLine
+        y={() => 50000}
+        style={{
+          data: {
+            strokeWidth: 1,
+            stroke: "#8cb1fa",
+            strokeDasharray: "5, 5"
+          }
+        }}
+      />
+      <VictoryLine
+        x={() => 300}
+        style={{
+          data: {
+            strokeWidth: 1,
+            stroke: "#ff8686",
+            strokeDasharray: "5, 5"
+          }
+        }}
+      />
+      <VictoryAxis dependentAxis style={{
+        axis: {
+          strokeWidth: 5,
+          stroke: "#5677fa"
+        },
+        grid: {
+          stroke: "#eef0fa"
+        }
+      }} />
+      <VictoryAxis style={{
+        axis: {
+          strokeWidth: 5,
+          stroke: "#5677fa"
+        }
+      }} />
+      <VictoryScatter
+        data={[
+          { x: 2442.24, y: 1920000, ...pointStyles.notSelected },
+          { x: 3538.08, y: 1920000, ...pointStyles.notSelected },
+          { x: 1234.08, y: 1920000, ...pointStyles.notSelected },
+          { x: 2315.52, y: 2160000, ...pointStyles.notSelected },
+          { x: 967.68, y: 1920000, ...pointStyles.notSelected },
+          { x: 356.832, y: 960000, ...pointStyles.notSelected },
+          { x: 1216.08, y: 1920000, ...pointStyles.notSelected },
+          { x: 1120.392, y: 1920000, ...pointStyles.notSelected },
+          { x: 362.88, y: 960000, ...pointStyles.notSelected },
+          { x: 892.08, y: 2400000, ...pointStyles.notSelected },
+          { x: 2959.2, y: 3840000, ...pointStyles.notSelected },
+          { x: 289.44, y: 240000, ...pointStyles.notSelected },
+          { x: 273.6, y: 480000, ...pointStyles.maxPerfWithCostLimit },
+          { x: 241.92, y: 480000, ...pointStyles.minCostWithPerfLimit },
+          { x: 725.76, y: 2160000, ...pointStyles.maxPerfOverCost },
+        ]}
+        size={10}
+        style={{ data: { strokeWidth: 2 } }}
+      />
+    </VictoryChart>
+  </div>
+);
+
+export default ResultFigure;
