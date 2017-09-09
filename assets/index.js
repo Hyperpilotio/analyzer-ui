f5aabb1b830b84eb8ea2096e5098358732be3c78
adrianliaw <adrianliaw2000@gmail.com>
2017-09-09 10:19:01 +0800

    Add index.js to assets to allow easy import of assets

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+export { default as loginLogo } from "./images/asset_hyperpilot_login_logo.svg";
+export { default as navLogo } from "./images/asset_hyperpilot_nav_logo.svg";
+export { default as kafkaLogo } from "./images/asset_kafka_logo.svg";
+export { default as mongoLogo } from "./images/asset_mongoDB_logo.svg";
+export { default as mysqlLogo } from "./images/asset_mysql_logo.svg";
+export { default as nginxLogo } from "./images/asset_nginx_logo.svg";
+export { default as gridViewIcon } from "./images/icon_grid_view.svg";
+export { default as listViewIcon } from "./images/icon_list_view.svg";
+export { default as mainMenuIcon } from "./images/icon_main_menu.svg";
+export { default as orgSettingIcon } from "./images/icon_org_setting.svg";
+export { default as analysisStageInactive } from "./images/icon_state_sizing_analysis.svg";
+export { default as analysisStageActive } from "./images/icon_state_sizing_analysis_active.svg";
+export { default as resultStageInactive } from "./images/icon_state_sizing_analysis_result.svg";
+export { default as resultStageActive } from "./images/icon_state_sizing_analysis_result_done.svg";
+export { default as userIcon } from "./images/icon_user.svg";
