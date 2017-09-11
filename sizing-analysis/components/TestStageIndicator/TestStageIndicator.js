93f608b86be388249d776f485510ac99a367b16f
adrianliaw <adrianliaw2000@gmail.com>
2017-09-11 15:03:42 +0800

    Fix eslint errors and warnings for TestStageIndicator

--- sizing-analysis/components/TestStageIndicator/TestStageIndicator.js
+++ sizing-analysis/components/TestStageIndicator/TestStageIndicator.js
@@ -1,24 +1,35 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./TestStageIndicator.scss";
 
-const TestStageIndicator = ({
-  title,
-  description,
-  icon,
-  activeIcon,
-  active = false
-}) => {
-  let activeClass = active ? styles.active : "";
-  let iconUrl = active ? activeIcon : icon;
-  return <div className={styles.TestStageIndicator}>
-    <img
-      className={`${styles.element} ${styles.icon} ${activeClass}`}
-      src={iconUrl} />
-    <div className={`${styles.element} ${styles.content} ${activeClass}`}>
-      <h4> { title } </h4>
-      <p> { description } </p>
+const TestStageIndicator = ({ title, description, icon, activeIcon, active }) => {
+  const activeClass = active ? styles.active : "";
+  const iconUrl = active ? activeIcon : icon;
+  return (
+    <div className={styles.TestStageIndicator}>
+      <img
+        className={`${styles.element} ${styles.icon} ${activeClass}`}
+        src={iconUrl}
+        alt={title}
+      />
+      <div className={`${styles.element} ${styles.content} ${activeClass}`}>
+        <h4> { title } </h4>
+        <p> { description } </p>
+      </div>
     </div>
-  </div>
-}
+  );
+};
+
+TestStageIndicator.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  activeIcon: PropTypes.string.isRequired,
+  active: PropTypes.bool,
+};
+
+TestStageIndicator.defaultProps = {
+  active: false,
+};
 
 export default TestStageIndicator;
