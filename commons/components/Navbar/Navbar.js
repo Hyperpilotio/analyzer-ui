8ed66dcb1519bc380414510b80ef484afedb82c8
adrianliaw <adrianliaw2000@gmail.com>
2017-09-11 10:09:53 +0800

    Fix eslint errors and warnings for Navbar

--- commons/components/Navbar/Navbar.js
+++ commons/components/Navbar/Navbar.js
@@ -1,13 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Container from "../Container";
 import styles from "./Navbar.scss";
 
-const Navbar = ({ className = "", children }) => (
+const Navbar = ({ className, children }) => (
   <nav className={`${styles.Navbar} ${className}`}>
     <Container className={styles.Container}>
       { children }
     </Container>
   </nav>
 );
+
+Navbar.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+Navbar.defaultProps = {
+  className: "",
+  children: null,
+};
 
 export default Navbar;
