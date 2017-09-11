e3f4e3d685423cd369be4b74e9ea78be315fc0f6
adrianliaw <adrianliaw2000@gmail.com>
2017-09-11 10:09:27 +0800

    Fix eslint errors and warnings for Jumbotron

--- commons/components/Jumbotron/Jumbotron.js
+++ commons/components/Jumbotron/Jumbotron.js
@@ -1,13 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Container from "../Container";
 import styles from "./Jumbotron.scss";
 
-const Jumbotron = ({ className = "", containerClassName = "", children }) => (
+const Jumbotron = ({ className, containerClassName, children }) => (
   <div className={`${styles.Jumbotron} ${className}`}>
     <Container className={`${styles.Container} ${containerClassName}`}>
       { children }
     </Container>
   </div>
 );
+
+Jumbotron.propTypes = {
+  className: PropTypes.string,
+  containerClassName: PropTypes.string,
+  children: PropTypes.node,
+};
+
+Jumbotron.defaultProps = {
+  className: "",
+  containerClassName: "",
+  children: null,
+};
 
 export default Jumbotron;
