90b8b4004ab7acb5a8d53ca064fcbf4567ba2042
adrianliaw <adrianliaw2000@gmail.com>
2017-09-11 10:10:03 +0800

    Fix eslint errors and warnings for ProgressBar

--- commons/components/ProgressBar/ProgressBar.js
+++ commons/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./ProgressBar.scss";
 
-const ProgressBar = ({ className = "", containerClass = "", percent = 100 }) => (
+const ProgressBar = ({ className, containerClass, percent }) => (
   <div className={`${styles.ProgressBar} ${containerClass}`}>
     <div
       style={{ width: `${percent}%` }}
-      className={`${styles["filling-area"]} ${className}`} />
+      className={`${styles["filling-area"]} ${className}`}
+    />
   </div>
 );
+
+ProgressBar.propTypes = {
+  className: PropTypes.string,
+  containerClass: PropTypes.string,
+  percent: PropTypes.number.isRequired,
+};
+
+ProgressBar.defaultProps = {
+  className: "",
+  containerClass: "",
+};
 
 export default ProgressBar;
