116a9b8f2cf24c7a50a0bdce1dcc2ea94d5786f7
adrianliaw <adrianliaw2000@gmail.com>
2017-09-11 10:09:43 +0800

    Fix eslint errors and warnings for NavItemLink

--- commons/components/NavItemLink/NavItemLink.js
+++ commons/components/NavItemLink/NavItemLink.js
@@ -1,21 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
+import stylePropType from "react-style-proptype";
 import { NavLink } from "react-router-dom";
 import _ from "lodash";
 import styles from "./NavItemLink.scss";
 
 
-export default ({
-  to,
-  text,
-  children,
-  className = "",
-  activeClassName = "",
-  activeStyle = {}
-}) => (
-  <NavLink to={to}
-           className={`${styles.NavItemLink} ${className}`}
-           activeClassName={`${styles.selected} ${activeClassName}`}
-           activeStyle={activeStyle}>
+const NavItemLink = ({ to, text, children, className, activeClassName, activeStyle }) => (
+  <NavLink
+    to={to}
+    className={`${styles.NavItemLink} ${className}`}
+    activeClassName={`${styles.selected} ${activeClassName}`}
+    activeStyle={activeStyle}
+  >
     { _.isUndefined(children) ? text : children }
   </NavLink>
-)
+);
+
+NavItemLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  text: PropTypes.string,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  activeClassName: PropTypes.string,
+  activeStyle: stylePropType,
+};
+
+NavItemLink.defaultProps = {
+  text: undefined,
+  children: undefined,
+  className: "",
+  activeClassName: "",
+  activeStyle: {},
+};
+
+export default NavItemLink;
