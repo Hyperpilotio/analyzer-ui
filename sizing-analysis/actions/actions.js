26c1c0dfdb8faf6a6c11817b183671b760a5f7a4
adrianliaw <adrianliaw2000@gmail.com>
2017-09-11 20:48:48 +0800

    Fix eslint errors and warnings for actions

--- sizing-analysis/actions/actions.js
+++ sizing-analysis/actions/actions.js
@@ -4,7 +4,7 @@ const mapStateToProps = state => state;
 
 const mapDispatchToProps = dispatch => ({
   addAll: () => dispatch({ type: ADD_ALL }),
-  toggleSelected: appName => dispatch({ type: TOGGLE_SELECTED, appName })
-})
+  toggleSelected: appName => dispatch({ type: TOGGLE_SELECTED, appName }),
+});
 
 export { mapDispatchToProps, mapStateToProps };
