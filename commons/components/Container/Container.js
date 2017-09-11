d6fb6fe3680e408f6f0a6afcc9ae519257393e57
adrianliaw <adrianliaw2000@gmail.com>
2017-09-11 10:42:06 +0800

    Fix eslint errors and warnings for Container

--- commons/components/Container/Container.js
+++ commons/components/Container/Container.js
@@ -1,11 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./Container.scss";
 
 
-const Container = ({ className = "", flex = false, children }) => (
+const Container = ({ className, flex, children }) => (
   <div className={`${styles.Container} ${flex ? styles.flex : ""} ${className}`}>
     { children }
   </div>
 );
+
+Container.propTypes = {
+  className: PropTypes.string,
+  flex: PropTypes.bool,
+  children: PropTypes.node,
+};
+
+Container.defaultProps = {
+  className: "",
+  flex: false,
+  children: null,
+};
 
 export default Container;
