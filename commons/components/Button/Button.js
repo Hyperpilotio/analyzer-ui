1875e1f3818dc03108b6bb8ef16a8bb525b06e8a
adrianliaw <adrianliaw2000@gmail.com>
2017-09-11 10:42:00 +0800

    Fix eslint errors and warnings for Button

--- commons/components/Button/Button.js
+++ commons/components/Button/Button.js
@@ -1,13 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./Button.scss";
 
-const Button = ({ invert = false, className = "", onClick = () => {}, children }) => {
-  let invertClass = invert ? styles.invert : "";
+const Button = ({ invert, className, onClick, children }) => {
+  const invertClass = invert ? styles.invert : "";
   return (
-    <a onClick={onClick} className={`${styles.Button} ${invertClass} ${className}`}>
+    <a
+      className={`${styles.Button} ${invertClass} ${className}`}
+      role="presentation"
+      onClick={onClick}
+    >
       { children }
     </a>
   );
+};
+
+Button.propTypes = {
+  invert: PropTypes.bool,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  children: PropTypes.node,
+};
+
+Button.defaultProps = {
+  invert: false,
+  className: "",
+  onClick: () => {},
+  children: null,
 };
 
 export default Button;
