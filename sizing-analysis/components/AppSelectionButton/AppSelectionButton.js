a1206c29491013e875ae5abee8163b96a0732981
adrianliaw <adrianliaw2000@gmail.com>
2017-09-12 15:35:25 +0800

    Fix eslint errors and warnings for AppSelectionButton

--- sizing-analysis/components/AppSelectionButton/AppSelectionButton.js
+++ sizing-analysis/components/AppSelectionButton/AppSelectionButton.js
@@ -1,17 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Button from "~/commons/components/Button";
+import { AnalyzerPropTypes } from "../../reducers/sizingReducer";
 import styles from "./AppSelectionButton.scss";
 
 
 const AppSelectionButton = ({ selected, app, onClick }) => {
-  let selectedClass = selected ? styles.selected : "";
-  
+  const selectedClass = selected ? styles.selected : "";
+
   return (
-    <Button className={ `${styles.AppSelectionButton} ${selectedClass}` } onClick={ onClick }>
-      <img src={ app.logo } />
+    <Button className={`${styles.AppSelectionButton} ${selectedClass}`} onClick={onClick}>
+      <img src={app.logo} alt={app.displayName} />
       { app.displayName }
     </Button>
   );
-}
+};
+
+AppSelectionButton.propTypes = {
+  app: AnalyzerPropTypes.app.isRequired,
+  selected: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+AppSelectionButton.defaultProps = {
+  selected: false,
+  onClick: () => {},
+};
 
 export default AppSelectionButton;
