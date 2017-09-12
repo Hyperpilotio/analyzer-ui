8d57ce16aeae240e9051c9e3d691dc62b4308425
adrianliaw <adrianliaw2000@gmail.com>
2017-09-12 15:39:22 +0800

    Fix eslint errors and warnings for sizing-analysis/App

--- sizing-analysis/App.js
+++ sizing-analysis/App.js
@@ -3,28 +3,39 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-d
 import Header from "../commons/components/Header";
 import SizingRunnerPage from "./components/SizingRunnerPage";
 import SizingResultsPage from "./components/SizingResultsPage";
-import { STAGE_CONFIG, STAGE_TEST, STAGE_RESULT } from "./constants";
+import { STAGE_CONFIG, STAGE_TEST } from "./constants";
 
 
 export default () => (
   <Router>
     <Switch>
-      <Route path="/" children={({ history }) => (
+      <Route path="/">
         <div>
           <Header />
           <Switch>
             <Route path="/sizing-test">
               <Switch>
-                <Route path="/sizing-test/run-test" render={props => <SizingRunnerPage stage={STAGE_TEST} {...props} />} />
-                <Route path="/sizing-test/result/:appName" component={SizingResultsPage} />
-                <Route path="/sizing-test/result" component={SizingResultsPage} />
-                <Route render={props => <SizingRunnerPage stage={STAGE_CONFIG} {...props} />} />
+                <Route
+                  path="/sizing-test/run-test"
+                  render={props => <SizingRunnerPage stage={STAGE_TEST} {...props} />}
+                />
+                <Route
+                  path="/sizing-test/result/:appName"
+                  component={SizingResultsPage}
+                />
+                <Route
+                  path="/sizing-test/result"
+                  component={SizingResultsPage}
+                />
+                <Route
+                  render={props => <SizingRunnerPage stage={STAGE_CONFIG} {...props} />}
+                />
               </Switch>
             </Route>
             <Redirect from="/" to="/sizing-test" />
           </Switch>
         </div>
-      )} />
+      </Route>
     </Switch>
   </Router>
-)
+);
