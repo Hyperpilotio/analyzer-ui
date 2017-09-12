d1a75ba02ff9f83e938473e34a88c28df7505171
adrianliaw <adrianliaw2000@gmail.com>
2017-09-12 15:32:32 +0800

    Fix eslint errors and warnings for AppSelector

--- sizing-analysis/components/AppSelector/AppSelector.js
+++ sizing-analysis/components/AppSelector/AppSelector.js
@@ -1,28 +1,39 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import Button from "~/commons/components/Button";
+import { AnalyzerPropTypes } from "../../reducers/sizingReducer";
 import AppSelectionButton from "../AppSelectionButton";
 import styles from "./AppSelector.scss";
-import { connect } from 'react-redux';
 import { mapStateToProps, mapDispatchToProps } from "../../actions";
 
 
-const AppSelector = ({ addAll, apps, toggleSelected, onAnalyze }) =>  (
+const AppSelector = ({ addAll, apps, toggleSelected, onAnalyze }) => (
   <div className={styles.AppSelector}>
     <div className={styles["select-buttons"]}>
-      <Button invert onClick={ onAnalyze }> Analyze </Button>
-      <Button onClick={ addAll }> Select All </Button>
+      <Button invert onClick={onAnalyze}> Analyze </Button>
+      <Button onClick={addAll}> Select All </Button>
     </div>
     <div className={styles["apps-area"]}>
-      { apps.map(app => (
-        <AppSelectionButton
-          key={ app.name }
-          selected={ app.selected }
-          app={ app }
-          onClick={ () => toggleSelected(app.name) }
-        />
-      )) }
+      {
+        apps.map(app => (
+          <AppSelectionButton
+            key={app.name}
+            selected={app.selected}
+            app={app}
+            onClick={() => toggleSelected(app.name)}
+          />
+        ))
+      }
     </div>
   </div>
 );
+
+AppSelector.propTypes = {
+  addAll: PropTypes.func.isRequired,
+  toggleSelected: PropTypes.func.isRequired,
+  onAnalyze: PropTypes.func.isRequired,
+  apps: PropTypes.arrayOf(AnalyzerPropTypes.app).isRequired,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(AppSelector);
