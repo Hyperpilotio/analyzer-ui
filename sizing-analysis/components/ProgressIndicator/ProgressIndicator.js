03337d63088adba31c50683c92aa622ed5df2602
adrianliaw <adrianliaw2000@gmail.com>
2017-09-12 15:26:18 +0800

    Fix eslint errors and warnings for ProgressIndicator

--- sizing-analysis/components/ProgressIndicator/ProgressIndicator.js
+++ sizing-analysis/components/ProgressIndicator/ProgressIndicator.js
@@ -1,29 +1,41 @@
 import React from "react";
-import TestStageIndicator from "../TestStageIndicator";
+import PropTypes from "prop-types";
 import testStageIcon from "~/assets/images/icon_state_sizing_analysis.svg";
 import testStageActiveIcon from "~/assets/images/icon_state_sizing_analysis_active.svg";
 import resultStageIcon from "~/assets/images/icon_state_sizing_analysis_result.svg";
 import resultStageActiveIcon from "~/assets/images/icon_state_sizing_analysis_result_done.svg";
+import TestStageIndicator from "../TestStageIndicator";
 import { STAGE_CONFIG, STAGE_TEST, STAGE_RESULT } from "../../constants";
 import styles from "./ProgressIndicator.scss";
 
 
-const ProgressIndicator = ({ stage = STAGE_CONFIG, className = "" }) => (
+const ProgressIndicator = ({ stage, className }) => (
   <div className={`${styles.ProgressIndicator} ${className}`}>
     <TestStageIndicator
       active={stage > STAGE_CONFIG}
       title="Sizing Analysis"
       description="app instances"
       icon={testStageIcon}
-      activeIcon={testStageActiveIcon} />
+      activeIcon={testStageActiveIcon}
+    />
 
     <TestStageIndicator
       active={stage > STAGE_TEST}
       title="Recommend"
       description="best instances"
       icon={resultStageIcon}
-      activeIcon={resultStageActiveIcon} />
+      activeIcon={resultStageActiveIcon}
+    />
   </div>
 );
+
+ProgressIndicator.propTypes = {
+  stage: PropTypes.oneOf([STAGE_CONFIG, STAGE_TEST, STAGE_RESULT]).isRequired,
+  className: PropTypes.string,
+};
+
+ProgressIndicator.defaultProps = {
+  className: "",
+};
 
 export default ProgressIndicator;
