03d85f28753a28c748ffd534e0380bf78f9ca1cb
adrianliaw <adrianliaw2000@gmail.com>
2017-09-12 12:03:34 +0800

    Fix eslint errors and warnings for RunnerModal

--- sizing-analysis/components/RunnerModal/RunnerModal.js
+++ sizing-analysis/components/RunnerModal/RunnerModal.js
@@ -1,68 +1,59 @@
 import React from "react";
-import FaCheck from "react-icons/lib/fa/check";
-import FaRefresh from "react-icons/lib/fa/refresh";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import ProgressBar from "~/commons/components/ProgressBar";
-import ProgressItem from "../ProgressItem";
-import Button from "~/commons/components/Button";
-import styles from "./RunnerModal.scss";
 import mysqlLogo from "~/assets/images/asset_mysql_logo.svg";
+import ProgressItem from "../ProgressItem";
+import { AnalyzerPropTypes } from "../../reducers/sizingReducer";
+import styles from "./RunnerModal.scss";
 
 
-const calculatePercentage = data => {
+const calculatePercentage = (data) => {
   if (data.status !== "complete" && data.sizingRuns.length === 0) {
     return 0;
   }
   if (data.status === "complete") {
     return 100;
-  } else {
-    let lastEntry = data.sizingRuns[data.sizingRuns.length - 1];
-    let generalProgress = Math.min(data.sizingRuns.length / 6, 0.9);
-    if (!!lastEntry && lastEntry.results[0].status === "running") {
-      return (generalProgress - (1 / 6) * 0.5) * 100;
-    } else {
-      return generalProgress * 100;
-    }
   }
-}
+  const lastEntry = data.sizingRuns[data.sizingRuns.length - 1];
+  const generalProgress = Math.min(data.sizingRuns.length / 6, 0.9);
+  if (!!lastEntry && lastEntry.results[0].status === "running") {
+    return (generalProgress - ((1 / 6) * 0.5)) * 100;
+  }
+  return generalProgress * 100;
+};
 
-const RunnerModal = ({
-  data = { status: "complete", sizingRuns: [] },
-  className = ""
-}) => (
+const RunnerModal = ({ data, className }) => (
   <div className={`${styles.RunnerModal} ${className}`}>
 
     <div className={styles.RunnerHeader}>
-      <img className={styles["RunnerHeader-logo"]} src={mysqlLogo} />
+      <img className={styles["RunnerHeader-logo"]} src={mysqlLogo} alt="MySQL" />
       MySQL running
     </div>
 
-    {/* progress bar*/}
+    {/* progress bar */}
     <div className={styles.progressbar}>
       <ProgressBar percent={calculatePercentage(data)} />
     </div>
 
 
-    {/* instance info*/}
+    {/* instance info */}
     <div className={styles["instance-progress-type"]}>
-
-    <div className={styles["instance-progress-type-status"]}>Status</div>
-    <div className={styles["instance-progress-type-perf"]}>Perf</div>
-    <div className={styles["instance-progress-type-cost"]}>Cost</div>
-        <div className={styles["instance-progress-type-perfcost"]}>Perf/Cost</div>
+      <div className={styles["instance-progress-type-status"]}>Status</div>
+      <div className={styles["instance-progress-type-perf"]}>Perf</div>
+      <div className={styles["instance-progress-type-cost"]}>Cost</div>
+      <div className={styles["instance-progress-type-perfcost"]}>Perf/Cost</div>
     </div>
 
-    {/* instance run*/}
+    {/* instance run */}
     <div className={styles["completion-status-group-canvas"]}>
-    <ul className={styles["completion-status-group"]}>
-      {
-        data.sizingRuns.map(({ results }) => (
-          results.map(result => (
-            <ProgressItem {...result} />
-          )
-        )))
-      }
-    </ul>
+      <ul className={styles["completion-status-group"]}>
+        {
+          data.sizingRuns.map(({ results }) => (
+            results.map(result => <ProgressItem {...result} />)
+          ))
+        }
+      </ul>
     </div>
 
 
@@ -73,5 +64,15 @@ const RunnerModal = ({
     </div>
   </div>
 );
+
+RunnerModal.propTypes = {
+  data: AnalyzerPropTypes.sizingRun,
+  className: PropTypes.string,
+};
+
+RunnerModal.defaultProps = {
+  data: { status: "complete", sizingRuns: [] },
+  className: "",
+};
 
 export default RunnerModal;
