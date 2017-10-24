300bc5eaf40a315f909e8692a56e54391b4ce5a7
adrianliaw <adrianliaw2000@gmail.com>
2017-10-25 01:55:14 +0800

    Adjust SetupPage layout and add selected applications area

--- alpha/containers/SetupPage.js
+++ alpha/containers/SetupPage.js
@@ -5,8 +5,10 @@ import ProgressBar from "~/commons/components/ProgressBar";
 const SetupPage = () => (
   <div className="container">
     <div className="row pt-5">
-      <h3>SETUP HYPERPILOT</h3>
-      <ProgressBar percent={33} />
+      <div className="col">
+        <h3>SETUP HYPERPILOT</h3>
+        <ProgressBar percent={33} />
+      </div>
     </div>
     <div className="row pt-3">
       <div className="col">
@@ -17,15 +19,15 @@ const SetupPage = () => (
         </nav>
       </div>
     </div>
-    <div className="row pt-5">
+    <div className="row pt-4 pb-1">
       <div className="col-sm-12">
         <h4>Step 1: Add Applications to HyperPilot</h4>
       </div>
     </div>
-    <div className="row pt-3" style={{ height: "300px", overflow: "scroll" }}>
+    <div className="row" style={{ height: "250px", overflow: "scroll" }}>
       {
-        ["MongoDB", "Billing Service", "Internal Dashboard", "Cassandra"].map(app => (
-          <div className="col-3" key={app}>
+        ["MongoDB", "Frontend Service", "Internal Dashboard", "Cassandra", "Jenkins"].map(app => (
+          <div className="col-3 pt-2 pb-2" key={app}>
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title">{ app }</h5>
@@ -37,9 +39,27 @@ const SetupPage = () => (
       }
     </div>
     <hr />
-    <div className="row">
+    <div className="row pt-2">
       <div className="col">
-        <h4>Selected Applications</h4>
+        <h4 className="text-secondary">Selected Applications</h4>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-9 p-0">
+        <div className="row m-0">
+          <div className="col-4 pt-2 pb-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Spark</h5>
+                <a href="#" className="card-link">Remove</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-3">
+        <h5 className="text-secondary">Done, next step:</h5>
+        <a className="btn btn-primary" href="#">Define SLO</a>
       </div>
     </div>
   </div>
