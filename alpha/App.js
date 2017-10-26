bf386dea450663327d4cb194611ddbbbfcce9fc2
adrianliaw <adrianliaw2000@gmail.com>
2017-10-26 17:52:05 +0800

    Add redux devtools

--- alpha/App.js
+++ alpha/App.js
@@ -8,20 +8,28 @@ import SetupPage from "./containers/SetupPage";
 import DashboardPage from "./containers/DashboardPage";
 
 
-const store = createStore(reducer, {
-  apps: [
-    { name: "goddd", id: "1" },
-    { name: "lucene", id: "2" },
-    { name: "kafka", id: "3" },
-    { name: "memcached", id: "4" },
-    { name: "mongo", id: "5" },
-    { name: "mysql", id: "6" },
-    { name: "nginx", id: "7" },
-    { name: "spark", id: "8" },
-    { name: "dashboard", id: "9" },
-  ],
-  addedAppIds: [],
-});
+const store = createStore(
+  reducer,
+  {
+    apps: [
+      { name: "goddd", id: "1" },
+      { name: "lucene", id: "2" },
+      { name: "kafka", id: "3" },
+      { name: "memcached", id: "4" },
+      { name: "mongo", id: "5" },
+      { name: "mysql", id: "6" },
+      { name: "nginx", id: "7" },
+      { name: "spark", id: "8" },
+      { name: "dashboard", id: "9" },
+    ],
+    addedAppIds: [],
+  },
+  /* eslint-disable no-underscore-dangle */
+  process.env.NODE_ENV === "production"
+    ? undefined
+    : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  /* eslint-enable */
+);
 
 export default () => (
   <Provider store={store}>
