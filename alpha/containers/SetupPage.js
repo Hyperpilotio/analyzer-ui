78c5a09c5277b895caf15c39836ce25fbad0277e
adrianliaw <adrianliaw2000@gmail.com>
2017-10-27 14:53:32 +0800

    Add loading data to SetupPage's componentWillMount

--- alpha/containers/SetupPage.js
+++ alpha/containers/SetupPage.js
@@ -1,24 +1,42 @@
 import React from "react";
+import { connect } from "react-redux";
 import ProgressBar from "~/commons/components/ProgressBar";
+import { fetchApps } from "../actions";
 
-const SetupPage = () => (
-  <div className="container">
-    <div className="row pt-5">
-      <div className="col">
-        <h3>SETUP HYPERPILOT</h3>
-        <ProgressBar percent={33} />
-      </div>
-    </div>
-    <div className="row pt-3">
-      <div className="col">
-        <ul className="nav nav-secondary nav-pills nav-fill">
-          <li className="nav-item nav-link text-primary">Step 1: Select Applications</li>
-          <li className="nav-item nav-link text-secondary">Step 2: Define SLO</li>
-          <li className="nav-item nav-link text-secondary">Step 3: Begin HyperPiloting</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-);
 
-export default SetupPage;
+class SetupPage extends React.Component {
+  componentWillMount() {
+    this.props.fetchApps();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row pt-5">
+          <div className="col">
+            <h3>SETUP HYPERPILOT</h3>
+            <ProgressBar percent={33} />
+          </div>
+        </div>
+        <div className="row pt-3">
+          <div className="col">
+            <ul className="nav nav-secondary nav-pills nav-fill">
+              <li className="nav-item nav-link text-primary">Step 1: Select Applications</li>
+              <li className="nav-item nav-link text-secondary">Step 2: Define SLO</li>
+              <li className="nav-item nav-link text-secondary">Step 3: Begin HyperPiloting</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  fetchApps: () => dispatch(fetchApps()),
+});
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(SetupPage)
