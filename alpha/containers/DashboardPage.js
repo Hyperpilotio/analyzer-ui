7267dc81505cd460ceab8772a2eef81bb74ca6b7
adrianliaw <adrianliaw2000@gmail.com>
2017-10-30 17:54:05 +0800

    Show list of apps in dashboard and services of an app

--- alpha/containers/DashboardPage.js
+++ alpha/containers/DashboardPage.js
@@ -1,6 +1,59 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Container, Row, Col, Card, CardBody, CardTitle } from "reactstrap";
+import { fetchApps } from "../actions";
+import { app as appPropType } from "../constants/propTypes";
 
 
-const DashboardPage = () => <div />;
+class DashboardPage extends React.Component {
+  static propTypes = {
+    fetchApps: PropTypes.func.isRequired,
+    apps: PropTypes.arrayOf(appPropType).isRequired,
+  }
 
-export default DashboardPage;
+  componentWillMount() {
+    this.props.fetchApps();
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row className="pt-4 pb-3">
+          <Col>
+            <h3>Applications managed by HyperPilot</h3>
+          </Col>
+        </Row>
+        <Row>
+          {
+            this.props.apps.map(app => (
+              <Col key={app._id} md={3}>
+                <Card>
+                  <CardBody>
+                    <CardTitle>{ app.name }</CardTitle>
+                    <ul>
+                      { app.serviceNames.map(service => <li>{ service }</li>) }
+                    </ul>
+                  </CardBody>
+                </Card>
+              </Col>
+            ))
+          }
+        </Row>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = ({ setup: { apps } }) => ({
+  apps,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchApps: () => dispatch(fetchApps()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(DashboardPage);
