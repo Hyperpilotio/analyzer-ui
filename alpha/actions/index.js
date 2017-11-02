fa90e008c4d05b4788f654ff1918480b9b825877
adrianliaw <adrianliaw2000@gmail.com>
2017-11-02 23:53:29 +0800

    Revert change in api url

--- alpha/actions/index.js
+++ alpha/actions/index.js
@@ -106,7 +106,7 @@ export const beginHyperPilotingFail = () => ({
 export const beginHyperPiloting = () => async (dispatch) => {
   dispatch(beginHyperPilotingLoading());
   // TODO: replace url when DB is done
-  const res = await fetch("http://localhost:3000/api/apps");
+  const res = await fetch("http://localhost:3007/begin");
   if (!res.ok) {
     dispatch(beginHyperPilotingFail());
     return;
