5498dc116e1989449901ec47d5825cd8d19060ba
Toby Chung <toby4120@gmail.com>
2017-11-02 11:47:47 +0800

    delete progress bar

--- alpha/containers/SetupPage.js
+++ alpha/containers/SetupPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import ProgressBar from "~/commons/components/ProgressBar";
+import { Button, Table } from "reactstrap";
 import { fetchApps } from "../actions";
 
 
@@ -17,21 +17,6 @@ class SetupPage extends React.Component {
 
     return (
       <div className="container">
-        <div className="row pt-5">
-          <div className="col">
-            <h3>SETUP HYPERPILOT</h3>
-            <ProgressBar percent={stepPercent} />
-          </div>
-        </div>
-        <div className="row pt-3">
-          <div className="col">
-            <ul className="nav nav-secondary nav-pills nav-fill">
-              <li className={`nav-item nav-link ${stepPercent > 0 ? "text-primary" : "text-secondary"}`}>Step 1: Select Applications</li>
-              <li className={`nav-item nav-link ${stepPercent > 34 ? "text-primary" : "text-secondary"}`}>Step 2: Define SLO</li>
-              <li className={`nav-item nav-link ${stepPercent > 70 ? "text-primary" : "text-secondary"}`}>Step 3: Begin HyperPiloting</li>
-            </ul>
-          </div>
-        </div>
       </div>
     );
   }
