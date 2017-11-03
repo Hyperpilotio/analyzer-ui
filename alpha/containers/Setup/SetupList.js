4d9fb025a22ebc0783c360972192ecac4424c7dc
Toby Chung <toby4120@gmail.com>
2017-11-03 13:34:05 +0800

    modified setup directory

---
+++ alpha/containers/Setup/SetupList.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { Table, Button } from "reactstrap";
+import MdEdit from "react-icons/lib/md/edit";
+import MdDelete from "react-icons/lib/md/cancel";
+import { fetchApps, resetStepNumber } from "../../actions";
+import _s from "../style.scss";
+
+class SetupList extends React.Component {
+  componentWillMount() {
+    this.props.fetchApps();
+    this.props.resetStep();
+  }
+
+  render() {
+    const {
+      apps,
+    } = this.props;
+    return (
+      <div className="container">
+        <div className="row pt-5">
+          <Link
+            to={"/setup/add"}
+            className="btn btn-primary mt-5 mb-2"
+            color="success"
+          >
+            + Add
+          </Link>
+        </div>
+
+        <div className="row">
+          <Table hover >
+            <thead>
+              <tr>
+                <th>APP Name</th>
+                <th>Services</th>
+                <th>SLO</th>
+                <th>STATE</th>
+                <th>{null}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                apps.map(d => (
+                  <tr key={d._id}>
+                    <td>{d.name}</td>
+                    <td>{d.services.length}</td>
+                    <td>{d.slo.type}: {d.slo.value} {d.slo.unit}</td>
+                    <td>{d.state}</td>
+                    <td>
+                      <Link to={`/setup/edit?iid=${d._id}`}><MdEdit className={`mr-3 ${_s.iconGrp}`} /></Link>
+                      <Link to="/"><MdDelete className={_s.iconGrp} /></Link>
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </Table>
+        </div>
+        <div className="row mt-4">
+          <Button className={`btn btn-success ${_s.btnBegin}`}>
+            <Link to="/setup/done">
+              Begin Hyperpiloting
+            </Link>
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+SetupList.propTypes = {
+  apps: PropTypes.arrayOf(
+
+  ).isRequired,
+  fetchApps: PropTypes.func.isRequired,
+  resetStep: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = ({ setup }) => ({
+  stepPercent: setup.stepPercent,
+  apps: setup.apps,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchApps: () => dispatch(fetchApps()),
+  resetStep: () => dispatch(resetStepNumber()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SetupList);
---
+++ alpha/containers/Setup/StepDone.js
@@ -0,0 +1,76 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { Container, Button } from "reactstrap";
+import { beginHyperPiloting } from "../../actions";
+import Spinner from "../../components/Spinner";
+import CheckMark from "../../components/CheckMark";
+import _s from "../style.scss";
+
+class SetupDone extends React.Component {
+  componentWillMount() {
+    this.props.beginHPClick();
+  }
+  render() {
+    const {
+      isBeginHyperPilotingLoading,
+      isHPReady,
+    } = this.props;
+    return (
+      <Container className="effect-fade-in">
+
+        { isBeginHyperPilotingLoading ?
+          <div className={`spinner-con loading ${_s.spinnerCon}`}>
+            <Spinner />
+          </div>
+          : null
+        }
+        { !isBeginHyperPilotingLoading && isHPReady ?
+          <div className={`check-con done ${_s.checkCon}`}>
+            <CheckMark />
+          </div>
+          : null
+        }
+        {
+          isBeginHyperPilotingLoading ?
+            <h4 className={_s.wordLoading}>Setting up HyperPilot ...</h4> : null
+        }
+        {
+          !isBeginHyperPilotingLoading && isHPReady ?
+            <div>
+              <h4 className={_s.wordSuccess}>HyperPilot is now running! </h4> 
+              <div className="row">
+                <Button className={`btn btn-success mt-3 ${_s.btnReturn}`}>
+                  <Link to="/setup">
+                    Back to App list
+                  </Link>
+                </Button>
+              </div>
+            </div>
+            : null
+        }
+      </Container>
+    );
+  }
+}
+
+SetupDone.propTypes = {
+  isBeginHyperPilotingLoading: PropTypes.bool.isRequired,
+  isHPReady: PropTypes.bool.isRequired,
+  beginHPClick: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = ({ setup }) => ({
+  isBeginHyperPilotingLoading: setup.ui.isBeginHyperPilotingLoading,
+  isHPReady: setup.isHPReady,
+});
+
+const mapDispatchToProps = dispatch => ({
+  beginHPClick: () => dispatch(beginHyperPiloting()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SetupDone);
