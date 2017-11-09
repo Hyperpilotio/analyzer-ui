eab2614ff5172da90fd569daa0560c7fd964179c
Toby Chung <toby4120@gmail.com>
2017-11-09 17:11:10 +0800

    done modifying card style to list cards in step two

--- alpha/containers/Setup/Step/StepTwo.js
+++ alpha/containers/Setup/Step/StepTwo.js
@@ -1,101 +1,102 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {
-  TabContent, TabPane,
-  Nav, NavItem, NavLink,
-  Card, CardText, CardBody,
-  CardTitle, Button,
+  Card, CardBody,
+  CardTitle, Button, Table,
 } from "reactstrap";
 import ReactRouterPropTypes from "react-router-prop-types";
 import { Link } from "react-router-dom";
-import classnames from "classnames";
 import _s from "../style.scss";
 import { app as appPropType } from "../../../constants/propTypes";
 
-const tabsArr = [
-  {
-    id: "1",
-    navLink: "All",
-  }, {
-    id: "2",
-    navLink: "Services",
-  }, {
-    id: "3",
-    navLink: "Deployments",
-  }, {
-    id: "4",
-    navLink: "Stateful Sets",
-  },
-];
+// const tabsArr = [
+//   {
+//     id: "1",
+//     navLink: "All",
+//   }, {
+//     id: "2",
+//     navLink: "Services",
+//   }, {
+//     id: "3",
+//     navLink: "Deployments",
+//   }, {
+//     id: "4",
+//     navLink: "Stateful Sets",
+//   },
+// ];
 
 const StepTwo = props => (
   <div>
-    <div className="row" >
-      <div className={_s.selectedZone}>
-        <h4 className="text-secondary">Selected Applications</h4>
-        {
-          props.addedApps.map(app => (
-            <div key={app._id} className={`mr-3 ${_s.cardCon}`}>
-              <Card className={_s.card}>
-                <CardBody>
-                  <CardTitle>{app.name}</CardTitle>
-                  <CardText>Kind: {app.deployment_template.kind}</CardText>
-                  <CardText>State: {app.state}</CardText>
-                  <Button className={_s.btn} onClick={() => props.onRemoveClick(app._id)} color="danger">Remove</Button>
-                </CardBody>
-              </Card>
-            </div>
-          ))
+    {/* Selected Apps */}
+    <Card className={`row ${_s.card}`}>
+      <CardBody>
+        <CardTitle>Selected Applications</CardTitle>
+        <Table>
+          <thead className={_s.tHead}>
+            <tr>
+              <th>Name</th>
+              <th>Namespace</th>
+              <th>Kind</th>
+              <th>Status</th>
+              <th>Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            {
+              props.addedApps.map(app => (
+                <tr>
+                  <th scope="row">{app.name}</th>
+                  <td>NameSpace</td>
+                  <td>{app.deployment_template.kind}</td>
+                  <td>{app.state}</td>
+                  <td>Age</td>
+                  <td><Button className={_s.btn} onClick={() => props.onRemoveClick(app._id)} color="danger">Remove</Button></td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </Table>
+        { props.addedApps.length <= 0 ?
+          <div className="row">
+            <span className={_s.noData}>No Service selected.</span>
+          </div> : null
         }
-      </div>
-    </div>
-    <div className="row" >
-      <div className="selected-zone">
-        <h4 className="text-secondary">Detected K8S Resources</h4>
-        <div>
-          {/* Tabs Link */}
-          <Nav tabs>
+      </CardBody>
+    </Card>
+    {/* Unselected Apps */}
+    <Card className={`row mt-5 mb-5 ${_s.card}`}>
+      <CardBody>
+        <CardTitle>Detected K8S Resources</CardTitle>
+        <Table>
+          <thead className={_s.tHead}>
+            <tr>
+              <th>Name</th>
+              <th>Namespace</th>
+              <th>Kind</th>
+              <th>Status</th>
+              <th>Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
             {
-              tabsArr.map(tab => (
-                <NavItem key={tab.id} className={_s.navItem} >
-                  <NavLink
-                    className={classnames({ active: props.activeTab === tab.id })}
-                    onClick={() => { props.toggleTabs(tab.id); }}
-                  >
-                    {tab.navLink}
-                  </NavLink>
-                </NavItem>
+              props.availableApps.map(app => (
+                <tr>
+                  <th scope="row">{app.name}</th>
+                  <td>NameSpace</td>
+                  <td>{app.deployment_template.kind}</td>
+                  <td>{app.state}</td>
+                  <td>Age</td>
+                  <td><Button className={_s.btn} onClick={() => props.onAddClick(app._id)} color="success">Add</Button></td>
+                </tr>
               ))
             }
-          </Nav>
-          {/* Tabs Content */}
-          <TabContent activeTab={props.activeTab} className={_s.tabContent}>
-            {
-              tabsArr.map(tab => (
-                <TabPane key={tab.id} tabId={tab.id}>
-                  <div className="resources-data">
-                    {
-                      props.availableApps.map(app => (
-                        <div key={app._id} className={`mr-3 ${_s.cardCon}`}>
-                          <Card className={_s.card}>
-                            <CardBody>
-                              <CardTitle>{app.name}</CardTitle>
-                              <CardText>Kind: {app.deployment_template.kind}</CardText>
-                              <CardText>State: {app.state}</CardText>
-                              <Button className={_s.btn} onClick={() => props.onAddClick(app._id)} color="success">Add</Button>
-                            </CardBody>
-                          </Card>
-                        </div>
-                      ))
-                    }
-                  </div>
-                </TabPane>
-              ))
-            }
-          </TabContent>
-        </div>
-      </div>
-    </div>
+          </tbody>
+        </Table>
+      </CardBody>
+    </Card>
     <div className={_s.btnRow}>
       <Link to={`${props.match.url}/1`} className="btn btn-secondary mr-2">Back</Link>
       <Link to={`${props.match.url}/3`} className="btn btn-primary">Next</Link>
@@ -108,7 +109,6 @@ StepTwo.propTypes = {
   match: ReactRouterPropTypes.match.isRequired,
   addedApps: PropTypes.arrayOf(appPropType).isRequired,
   availableApps: PropTypes.arrayOf(appPropType).isRequired,
-  activeTab: PropTypes.string.isRequired,
 };
 
 
