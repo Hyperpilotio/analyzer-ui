2258477cc08a4d5a21a98b69fb02d6ff548a2349
adrianliaw <adrianliaw2000@gmail.com>
2017-12-28 20:09:35 +0800

    Fix webpack build error

--- sizing-analysis/App.js
+++ sizing-analysis/App.js
@@ -1,41 +1,59 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
 import Header from "../commons/components/Header";
 import SizingRunnerPage from "./components/SizingRunnerPage";
 import SizingResultsPage from "./components/SizingResultsPage";
 import { STAGE_CONFIG, STAGE_TEST } from "./constants";
+import "./index.scss";
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducers,
+  // Activate redux devtools in development mode
+  process.env.NODE_ENV === "production"
+    ? undefined
+    : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
+
+persistStore(store);
 
 
 export default () => (
-  <Router>
-    <Switch>
-      <Route path="/">
-        <div>
-          <Header />
-          <Switch>
-            <Route path="/sizing-test">
-              <Switch>
-                <Route
-                  path="/sizing-test/run-test"
-                  render={props => <SizingRunnerPage stage={STAGE_TEST} {...props} />}
-                />
-                <Route
-                  path="/sizing-test/result/:appName"
-                  component={SizingResultsPage}
-                />
-                <Route
-                  path="/sizing-test/result"
-                  component={SizingResultsPage}
-                />
-                <Route
-                  render={props => <SizingRunnerPage stage={STAGE_CONFIG} {...props} />}
-                />
-              </Switch>
-            </Route>
-            <Redirect from="/" to="/sizing-test" />
-          </Switch>
-        </div>
-      </Route>
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <div>
+            <Header />
+            <Switch>
+              <Route path="/sizing-test">
+                <Switch>
+                  <Route
+                    path="/sizing-test/run-test"
+                    render={props => <SizingRunnerPage stage={STAGE_TEST} {...props} />}
+                  />
+                  <Route
+                    path="/sizing-test/result/:appName"
+                    component={SizingResultsPage}
+                  />
+                  <Route
+                    path="/sizing-test/result"
+                    component={SizingResultsPage}
+                  />
+                  <Route
+                    render={props => <SizingRunnerPage stage={STAGE_CONFIG} {...props} />}
+                  />
+                </Switch>
+              </Route>
+              <Redirect from="/" to="/sizing-test" />
+            </Switch>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+  </Provider>
 );
--- sizing-analysis/index.js
+++ sizing-analysis/index.js
@@ -1,44 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { AppContainer as ReactHotLoaderContainer } from "react-hot-loader";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import { persistStore } from "redux-persist";
 import App from "./App";
-import reducers from "./reducers";
-import "./index.scss";
 
-/* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  // Activate redux devtools in development mode
-  process.env.NODE_ENV === "production"
-    ? undefined
-    : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
-/* eslint-enable */
-
-persistStore(store);
-
-const render = (Component) => {
-  const rootEl = document.getElementById("react-root");
-  ReactDOM.render(
-    <ReactHotLoaderContainer>
-      <Provider store={store}>
-        <Component />
-      </Provider>
-    </ReactHotLoaderContainer>,
-    rootEl,
-  );
-};
-
-render(App);
-
-if (process.env.NODE_ENV !== "production" && module.hot) {
-  /* eslint-disable no-shadow, global-require */
-  module.hot.accept("./App", () => {
-    const App = require("./App").default;
-    render(App);
-  });
-  /* eslint-enable */
-}
+const rootEl = document.getElementById("react-root");
+ReactDOM.render(<App />, rootEl);
