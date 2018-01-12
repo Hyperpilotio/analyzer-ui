c34b8556ea54b1a909d60a1687949fb0ea78d196
Toby Chung <toby4120@gmail.com>
2018-01-12 20:52:11 +0800

    add loading reducer for mock up

--- alpha/reducers/ui.js
+++ alpha/reducers/ui.js
@@ -1,22 +1,27 @@
 import _ from "lodash";
 import * as actionTypes from "../actions/types";
-import { LOADING, SUCCESS, FAIL } from "../constants/apiActions";
+import { LOADING, SUCCESS, FAIL, SINGLE_PENDING } from "../constants/apiActions";
 
+// 可以在這邊做決斷
 const RSAATypes = _.pickBy(actionTypes, type => _.isArray(type) && type.length === 3);
 
+const simpleStates = ["isLoading", "isFulfilled", "isRejected", "isSettled", "isRefreshing"];
+const trackableKeys = ["map"];
+
 const initialState = _.mapValues(
-  _.mapKeys(RSAATypes, (__, name) => _.camelCase(`IS_${name}_LOADING`)),
-  _.constant(false),
+  _.mapKeys(RSAATypes, (__, name) => (_.camelCase(`${name}`))),
+  () => (_.zipObject(simpleStates, _.map(simpleStates, () => (false)))),
 );
 
 export default (state = initialState, action) => {
-  const actionName = _.findKey(RSAATypes, types => types.includes(action.type));
+  const actionName = _.findKey(actionTypes, types => types.includes(action.type));
 
   if (_.isUndefined(actionName)) {
     return state;
   }
 
   const uiFieldName = _.camelCase(`IS_${actionName}_LOADING`);
+
   switch (actionTypes[actionName].indexOf(action.type)) {
   case LOADING:
     return {
@@ -38,6 +43,11 @@ export default (state = initialState, action) => {
     return {
       ...state,
       [uiFieldName]: false,
+    };
+  case SINGLE_PENDING:
+    return {
+      ...state,
+      [uiFieldName]: true,
     };
   default:
     return state;
